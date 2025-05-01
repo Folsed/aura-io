e5a0646c1ece217a2e5879446f96dd0aecafe60f
Kyrylo <dexter37814@gmail.com>
2025-05-01 21:53:58 +0300

    feat: air pollution progress bar

--- src/app/components/air-pollution/AirPollution.tsx
+++ src/app/components/air-pollution/AirPollution.tsx
@@ -1,11 +1,25 @@
 'use client'
+import { Progress } from '@/components/ui/progress'
 import { useGetAirPollutionDataQuery } from '@/store/features/weather/weatherApiSlice'
+import { airQulaityIndexText } from '@/utils/airQualityIndex'
 import { ThermometerSun } from 'lucide-react'
 
 const AirPollution = () => {
-    const { data, isLoading, isError } = useGetAirPollutionDataQuery({ lat: 50.4418, lon: 30.5104 })
+    const {
+        data: airPollution,
+        isLoading,
+        isError,
+    } = useGetAirPollutionDataQuery({ lat: 50.4418, lon: 30.5104 })
 
-    console.log(data)
+    if (isLoading) {
+        return <p>Is Loading...</p>
+    }
+
+    const airQualityIndex = Number(airPollution?.list[0].main.aqi) * 10
+
+    const filteredIndex = airQulaityIndexText.find(item => {
+        return item.rating === airQualityIndex
+    })
 
     return (
         <div className='air-pollution dark:bg-dark-grey sm-2:col-span-2 col-span-full flex h-[12rem] flex-col gap-8 rounded-lg border px-4 pt-6 shadow-sm md:col-span-2 xl:col-span-2 dark:shadow-none'>
@@ -13,8 +27,8 @@ const AirPollution = () => {
                 <ThermometerSun />
                 Air Pollusion
             </h2>
-            {/* <Progress value={airQualityIndex} max={100} className='progress' /> */}
-            {/* <p className='text-sm'>Air quality is {filteredIndex?.description}. </p> */}
+            <Progress value={airPollution?.list[0].main.aqi} max={100} className='progress' />
+            <p className='text-sm'>Air quality is {filteredIndex?.description}. </p>
         </div>
     )
 }
---
+++ src/utils/airQualityIndex.ts
@@ -0,0 +1,47 @@
+interface IAirQualityIndex {
+    rating: number
+    description: string
+}
+
+export const airQulaityIndexText: IAirQualityIndex[] = [
+    {
+        rating: 10,
+        description: 'excellent',
+    },
+    {
+        rating: 20,
+        description: 'good',
+    },
+    {
+        rating: 30,
+        description: 'satisfactory',
+    },
+    {
+        rating: 40,
+        description: 'fair',
+    },
+    {
+        rating: 50,
+        description: 'moderate',
+    },
+    {
+        rating: 60,
+        description: 'moderate',
+    },
+    {
+        rating: 70,
+        description: 'poor',
+    },
+    {
+        rating: 80,
+        description: 'poor',
+    },
+    {
+        rating: 90,
+        description: 'very poor',
+    },
+    {
+        rating: 100,
+        description: 'very poor',
+    },
+]
