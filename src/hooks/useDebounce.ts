b7b845a5e598743a92253d68910ab576c5285103
Kyrylo <dexter37814@gmail.com>
2025-05-03 20:50:54 +0300

    feat: add useDebounce hook

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,20 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+const useDebounce = <T>(value: T, delay: number) => {
+    const [debValue, setDebValue] = useState<T>(value)
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebValue(value)
+        }, delay);
+
+        return () => {
+            clearTimeout(handler)
+        }
+    }, [value, delay])
+    
+    return debValue
+}
+
+export default useDebounce
